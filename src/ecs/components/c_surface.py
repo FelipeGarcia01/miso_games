ba4a9b3f99eb31c60de689cbe146708b4681a952
felipe <lf.garciar1@uniandes.edu.co>
2024-04-27 11:25:43 -0500

    add surface to print text on screem

--- src/ecs/components/c_surface.py
+++ src/ecs/components/c_surface.py
@@ -8,6 +8,20 @@ class CSurface:
         self.area = self.surf.get_rect()
 
     @classmethod
+    def from_text(cls, text: str,
+                  font: str = None,
+                  font_color: pygame.Color = None,
+                  font_size: int = 10):
+        if font_color is None:
+            font_color = pygame.Color(255, 255, 255)
+
+        font_family = pygame.font.SysFont(font, font_size)
+        c_surf = cls(pygame.Vector2(1, 1), pygame.Color(0, 0, 0, 0))
+        c_surf.surf = font_family.render(text, True, font_color)
+        c_surf.area = c_surf.surf.get_rect()
+        return c_surf
+
+    @classmethod
     def from_surface(cls, surface: pygame.Surface):
         c_surf = cls(pygame.Vector2(0, 0), pygame.Color(0, 0, 0))
         c_surf.surf = surface
